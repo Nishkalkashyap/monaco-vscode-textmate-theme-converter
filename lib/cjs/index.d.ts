7ae6d1c47ce95d7c84067131f0d4dbb07fcefadb
Nishkal Kashyap <kashyapnishkal@gmail.com>
2021-08-07 13:06:53 +0530

    Created lib directory

--- lib/cjs/index.d.ts
+++ lib/cjs/index.d.ts
@@ -1,5 +1,6 @@
 import { IVSCodeTheme } from "./interfaces";
 import * as monaco from 'monaco-editor';
 export * from './interfaces';
+export declare function convertThemeFromFilePath(inputFilePath: string, outputFilePath: string): void;
 export declare function convertThemeFromDir(inputDir: string, outDir: string): void;
 export declare function convertTheme(theme: IVSCodeTheme): monaco.editor.IStandaloneThemeData;
