dd5d5248e16ae9d090637103bf439d090ab73308
Nishkal Kashyap <kashyapnishkal@gmail.com>
2021-08-07 13:40:07 +0530

    Added CLI usage

--- src/npx-script.ts
+++ src/npx-script.ts
@@ -5,7 +5,7 @@ import * as fs from 'fs-extra';
 import * as path from 'path';
 
 program
-  .version("0.1.5")
+  .version("0.1.6")
   .requiredOption('-i, --input <inputPath>', 'Specify a file or a folder')
   .option('-o, --output <outputPath>', 'Specify an output path');
 
