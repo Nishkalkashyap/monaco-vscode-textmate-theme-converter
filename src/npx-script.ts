f486f37ff045e028576b4566b55a0ede84ef964f
Nishkal Kashyap <kashyapnishkal@gmail.com>
2021-08-10 10:36:42 +0530

    Released version 0.1.7

--- src/npx-script.ts
+++ src/npx-script.ts
@@ -5,7 +5,7 @@ import * as fs from 'fs-extra';
 import * as path from 'path';
 
 program
-  .version("0.1.6")
+  .version("0.1.7")
   .requiredOption('-i, --input <inputPath>', 'Specify a file or a folder')
   .option('-o, --output <outputPath>', 'Specify an output path');
 
